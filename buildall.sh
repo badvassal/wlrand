a05dde9874bed07ef86dca8b56373e551350172d
badvassal <badvassal0@gmail.com>
2019-11-21 07:38:25 -0800

    buildall.sh: include git state in artifact names
    Also: compress a `wlrand` directory containing the executable and the
    readme (was: compress just the executable).

--- buildall.sh
+++ buildall.sh
@@ -1,18 +1,48 @@
 set -e
 
 version=$(make version)
+git_state=$(make gitstate)
+verstr=$version-$git_state
 
 rm -r release
 mkdir release
 
-make build GOOS=linux
-tar czvf "release/wlrand-$version-linux.tgz" wlrand 
-rm wlrand
+# Generates uncompressed artifacts in the `work/wlrand` directory.
+#
+# $1: OS name
+# $2: executable filename
+build_one() {
+    os=$1
+    exe=$2
 
-make build GOOS=darwin
-tar czvf "release/wlrand-$version-macos.tgz" wlrand 
-rm wlrand
+    rm -rf release/work
+    mkdir -p release/work/wlrand
 
-make build GOOS=windows
-zip "release/wlrand-$version-windows.zip" wlrand.exe
-rm wlrand.exe
+    make build GOOS="$os"
+    mv "$exe" release/work/wlrand
+    cp README.md release/work/wlrand
+}
+
+build_one linux wlrand
+(
+    artifact=wlrand-$verstr-linux.tgz
+    cd release/work
+    tar -czvf "$artifact" wlrand
+    mv "$artifact" ..
+)
+
+build_one darwin wlrand
+(
+    artifact=wlrand-$verstr-macos.tgz
+    cd release/work
+    tar -czvf "$artifact" wlrand
+    mv "$artifact" ..
+)
+
+build_one windows wlrand.exe
+(
+    artifact=wlrand-$verstr-windows.zip
+    cd release/work
+    zip -r "$artifact" wlrand
+    mv "$artifact" ..
+)
